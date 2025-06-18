8480097a35fa870c730c248541523af82055baad
Davidtch <giordani.david@gmail.com>
2025-06-18 23:15:34 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 1000;
+const port = process.env.PORT || 10000;
 
 app.use(bodyParser.json());
 
