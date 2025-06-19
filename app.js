78af42a7b86df9ba4c56d40d40ab0551fab94ff6
Davidtch <giordani.david@gmail.com>
2025-06-19 03:59:02 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // ou mets ici ton domaine spécifique
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
